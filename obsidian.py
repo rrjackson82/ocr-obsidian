01b3146c71bc9dc3e3349b59683bde181ebf51a6
Dane Ohr <danonator821@gmail.com>
2026-07-07 21:02:08 -0400

    Added note creation, fix bugs, and added many overall improvements

--- obsidian.py
+++ obsidian.py
@@ -1,7 +1,9 @@
+from datetime import datetime
 import fetch_settings
 from ollama_handler import text_prompt
 from pathlib import Path
 import subprocess
+from json import loads
 
 settings = fetch_settings.Settings.load()
 
@@ -14,25 +16,26 @@ def search_tags(vault: fetch_settings.Vault):
         capture_output=True, text=True
     )
     tags = list(set(result.stdout.splitlines()))
-
-    # for file in path.rglob(f"{path}/**/*.md"):
-    #     with open(file, "r") as f:
-    #         file_content = f.read()
-
+    vault.tags = tags
     return tags
 
 async def generate_file_data(vault: fetch_settings.Vault, content):
+    if not vault.tags:
+        search_tags(vault)
+    print(f"Vault tags: {vault.tags}")
     prompt = f"""Take the following file and add the following file-data:
     -File name
     -Tags
     Rules:
-    -File name should be relevant to the content on the page
-    -Do not include file extensions in file name-give name only
-    -Do not include dots (.) in filename. Replace spaces with hyphen (-)
-    -Do not edit the content in any way
-    -Only add tags if it is necessary
-    -Create a tag only if the options in the 'tags' section do not suffice 
-    -If you create a tag, switch 'createdTag' to True. Otherwise leave False
+    - File name should be relevant to the content on the page
+    - Do not include file extensions in file name-give name only
+    - Do not include dots '.' in filename. Replace spaces with '%^'
+    - Filename should be in title case
+    - Do not edit the content in any way
+    - Only add tags if it is necessary
+    - Tags must start with '#' and be all lowercase
+    - Create a tag only if the options in the 'tags' section are not related to the note contents
+
     
     Tags list:
     {vault.tags}
@@ -51,16 +54,36 @@ async def generate_file_data(vault: fetch_settings.Vault, content):
     }}
     
     """
-    data = text_prompt(prompt)
+    data = await text_prompt(prompt)
+    raw = data.message.content
+    raw = raw[raw.index('{'):raw.index('}')+1]
+    data = loads(raw)
+    print("\n---///---///---///---")
+    print(data)
+    print("---///---///---///---\n")
     filename = data["filename"]
     tags = data["tags"]
     createdTag = data["createdTag"]
-    return
+    if createdTag:
+        current_tags = search_tags(vault)
+        total_tags = list(dict.fromkeys(current_tags + (tags or []))) # Avoid duplicates
+        vault.tags = total_tags
+        settings.save()
+    return {
+        "filename": filename.replace('%^', " "),
+        "tags": tags,
+    }
 
-def create_note(vault: fetch_settings.Vault, content: str):
-    print('---///---')
-    print(f"Creating note '{vault.name}' in vault '{vault.name}'")
-    print('---///---')
+def create_note(vault: fetch_settings.Vault, content: str, filename: str, tags: list):
+    full_path = Path(vault.path) / f"{filename}.md"
+    if tags:
+        formatted_tags = f"{' '.join(tags)}"
+        pre_content = f"""Tags: {formatted_tags}\nCreated: {datetime.now().date()}"""
+    else:
+        pre_content = f"Created: {datetime.now().date()}"
+    final_content = f"""{pre_content}\n{content}"""
+
+    full_path.write_text(final_content)
 
 if __name__ == '__main__':
     vault = settings.get_vault(settings.default_vault)
--- telegram_bot.py
+++ telegram_bot.py
@@ -1,12 +1,12 @@
 # TODO add commands for obsidian vault scanning and viewing, available tags, AI endpoints etc
 
-import logging
-from urllib.parse import uses_relative
 
+import logging
 from telethon import TelegramClient, events, utils, Button
 from os import getenv
 from dotenv import load_dotenv
 import obsidian
+from tesseract_ocr import ocr_image_from_telethon
 from ollama_ocr import model_info, process_image
 from fetch_settings import Settings
 
@@ -22,92 +22,70 @@ client = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
 
 @client.on(events.NewMessage(pattern='/start'))
 async def start(event):
-    await event.respond('Hello! Begin by sending an image to be processed by the ocr or type /help for available commands')
+    await event.respond('Hello! Begin by sending an image to be processed by the ocr')
     logging.info(f'Start command received from {event.sender_id}')
 
 @client.on(events.NewMessage(pattern='/help'))
 async def help(event):
-    user_state[event.sender_id] = {
-        "step": "help"
-    }
-    help_text = ("""Hi :) Send images here to have it uploaded to an OCR tool, which scans for text in the image, converts it to markdown, and adds it to a chosen Obsidian vault
-
-/help: This command
-/settings: Tweak settings such as the AI model, AI endpoints, creating new vaults, and adding existing vaults
-/list: list all registered vaults.""")
+    help_text = ("Hi :) Send images here to have it uploaded to an OCR tool, which scans for text present in the image and adds it to a specified Obisidan vault",
+    "/help: This command",
+    "/settings: Tweak settings such as default OCR model (under development)")
     await event.respond(help_text)
     logging.info(f'Help command received from {event.sender_id}')
 
 @client.on(events.NewMessage(pattern="/settings"))
 async def settings(event):
-    buttons = [Button.inline("Change Model", b"Change Model"), Button.inline("Change Endpoint", b"Change Endpoint"),
-               Button.inline("Create New Vault", b"Create Vault")]
-    user_state[event.sender_id] = {
-        "step": "settings",
-        "function": ""
-    }
-    await event.respond("What would you like to do?", buttons=buttons)
+    await event.respond("Coming soon")
 
-@client.on(events.NewMessage(pattern="/list"))
-async def list_vaults(event):
-    vaults = list_vaults()
-    await event.respond(f"Your registered vaults:\n{vaults}")
-
-## WHEN USER SENDS IMAGE
 @client.on(events.NewMessage(func=lambda e: bool(e.media) and utils.is_image(e.media)))
 async def handle_image(event):
     user_state[event.sender_id] = {
-        "step": "generate_metadata",
+        "step": "ask_ocr_model",
         "chat_id": event.chat_id,
         "img_msg_id": event.id
     }
-    state = user_state.get(event.sender_id)
-    await event.respond(f"Step 1) Generating metadata from image using {model_info()['model']}, this may take some time...")
-
-    #generate metadata
-    response = await process_image(client, state["chat_id"], state["img_msg_id"])
-    # await event.respond(response)
-    await event.respond("Generated markdown from image.")
-    user_state[event.sender_id] = {
-        "step": "ask_obsidian_vault",
-        "markdown": response,
-    }
-
-    # choosing obsidian vault
-    buttons = []
-    usr_vaults = Settings.load().vaults
-    for vault in usr_vaults:
-        buttons.append(Button.inline(vault.name, f"{vault.name}".encode()))
-    await event.respond("Step 2) Vault selection", buttons=buttons)
+    await event.respond("""
+    Step 1) OCR model selection
+    \nPlease select which OCR model (image to text engine) you would like to use:
+    \n\ta) Tesseract (machine learning: faster, less accurate. Easiest to start)
+    \n\tb) Locally hosted AI (you host, add your own endpoint in /settings: Slower but more accurate)
+    \nRespond with 'a', 'b', 'tesseract', or 'ai'
+    """)
+    print(f"user state: {user_state}")
 
 @client.on(events.NewMessage)
 async def handle_followup(event):
     state = user_state.get(event.sender_id)
-    text = event.text
     if not state:
         await event.respond("User state err")
         return
+    
+    if state["step"] == "ask_ocr_model":
+        answer = event.text.lower()
 
-    # after user clicks the settings button
-    if state["step"] == "settings" and state["function"] != "":
-        match state["function"]:
-            case "change_model":
-                await event.respond(f"Setting model to {text}...")
-                obsidian.settings.ai_model = text
-                obsidian.settings.save()
-                await event.respond(f"Done! Set model to {text}.")
-            case "change_endpoint":
-                await event.respond(f"Changing Ollama endpoint to {text}...")
-                obsidian.settings.ai_endpoint = text
-                obsidian.settings.save()
-                await event.respond(f"Done! Set model to {text}.")
-    if state["step"] == "settings" and state["function"] == "create_vault":
-        match state["substep"]:
-            case "vault_name": # After user inputs the name of the vault
-                name = text
-                obsidian.settings.create_vault(name)
-                vault = obsidian.settings.get_vault(name)
-                await event.respond(f"Created vault '{name}' at path '{vault.path}'")
+        if answer.lower() in ['b', 'ai']:
+            await event.respond(f"You have selected to use AI ({model_info()['model']})")
+            await event.respond("Working with AI...")
+            response = await process_image(client, state["chat_id"], state["img_msg_id"])
+            await event.respond(response)
+            user_state[event.sender_id] = {
+                "step": "ask_obsidian_vault",
+                "markdown": response,
+            }
+            state = user_state.get(event.sender_id)
+        elif answer.lower() in ['a', 'tesseract']:
+            await event.respond("You have selected to use tesseract")
+            text = await ocr_image_from_telethon(client, state["chat_id"], state["img_msg_id"])
+            await event.respond(text)
+        # else:
+            # await event.respond("Response not in options")
+    if state["step"] == "ask_obsidian_vault":
+        buttons = []
+        usr_vaults = Settings.load().vaults
+        for vault in usr_vaults:
+            buttons.append(Button.inline(vault.name, f"{vault.name}".encode()))
+        await event.respond("Step 2) Vault selection", buttons=buttons)
+
 
 @client.on(events.CallbackQuery)
 async def handle_button(event):
@@ -119,64 +97,12 @@ async def handle_button(event):
         vault_name = data.decode()
         vault = obsidian.settings.get_vault(vault_name)
 
-        await event.respond(f"Generating file data (title, filename, etc). This may take some time...")
+        await event.respond(f"Selected '{vault_name}', generating file data (title, filename, etc)")
         metadata = await obsidian.generate_file_data(vault, state["markdown"])
+        await event.respond(f"Metadata:\n{metadata}")
         await event.respond("Adding to Vault...")
         obsidian.create_note(vault, state["markdown"], metadata['filename'], metadata['tags'])
         await event.respond(f"Created note {metadata['filename']}.md to vault '{vault_name}'")
-    elif state["step"] == "settings":
-        # change model, change endpoint, add vault
-        match data.decode().strip().lower():
-            case "change model":
-                await event.respond(f"""Ok, let's change the model.
-Please type in the exact model name.
- For example, Type 'qwen2.5vl:3b' for the 'qwen2.5vl:3b' model.
-This should be the same thing you type to download models with ollama:
-```ollama run <model-name>``` or ```ollama pull <model name>```
-For a list of all models installed on your machine, run ```ollama list``` in your terminal.
-Make sure you have the model actually installed!
-You are currently using {obsidian.settings.ai_model}
-Which model would you like to change to?""")
-                user_state[event.sender_id] = {
-                    "step": "settings",
-                    "function": "change_model"
-                }
-
-
-            case "change endpoint":
-                await event.respond(f"""You selected to change the endpoint. 
-You are currently using
-```{obsidian.settings.ai_endpoint}```
-
-Ollama's default is 
-```http://localhost:11434```
-
-Your endpoint should start with 'http://' or 'https://'. An example would be:
-```http://123.45.678.90:11434```
-
-Please type the endpoint you'd like to use. 
-""")
-                user_state[event.sender_id] = {
-                    "step": "settings",
-                    "function": "change_endpoint"
-                }
-
-
-            case "create vault":
-                vaults = ""
-                for vault in obsidian.settings.list_vaults():
-                    name = vault[0]
-                    vaults = vaults + "    __" + name + "__\n"
-                await event.respond(f"""You selected to add a vault.
-Your current vaults are:
-{vaults}
-""")
-                await event.respond("What would you like to name your vault? This will be the folder name.")
-                user_state[event.sender_id] = {
-                    "step": "settings",
-                    "function": "create_vault",
-                    "substep": "vault_name"
-                }
 
 
 
