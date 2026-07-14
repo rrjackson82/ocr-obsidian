d56335f59b164482114262b7aa98e4ab93811058
Dane Ohr <danonator821@gmail.com>
2026-07-14 17:14:11 -0400

    Added vault creation but buggy - finished settings for now

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,7 +1,8 @@
 # TODO add commands for obsidian vault scanning and viewing, available tags, AI endpoints etc
 
-
 import logging
+from urllib.parse import uses_relative
+
 from telethon import TelegramClient, events, utils, Button
 from os import getenv
 from dotenv import load_dotenv
@@ -34,8 +35,20 @@ async def help(event):
 
 @client.on(events.NewMessage(pattern="/settings"))
 async def settings(event):
-    await event.respond("Coming soon")
+    buttons = [Button.inline("Change Model", b"Change Model"), Button.inline("Change Endpoint", b"Change Endpoint"),
+               Button.inline("Create New Vault", b"Create Vault")]
+    user_state[event.sender_id] = {
+        "step": "settings",
+        "function": ""
+    }
+    await event.respond("What would you like to do?", buttons=buttons)
 
+@client.on(events.NewMessage(pattern="/list"))
+async def list_vaults(event):
+    vaults = list_vaults()
+    await event.respond(f"Your registered vaults:\n{vaults}")
+
+## WHEN USER SENDS IMAGE
 @client.on(events.NewMessage(func=lambda e: bool(e.media) and utils.is_image(e.media)))
 async def handle_image(event):
     user_state[event.sender_id] = {
@@ -44,12 +57,12 @@ async def handle_image(event):
         "img_msg_id": event.id
     }
     state = user_state.get(event.sender_id)
-    await event.respond(f"Step 1) Generating metadata from image using {model_info()['model']}...")
-    print(f"user state: {user_state}")
+    await event.respond(f"Step 1) Generating metadata from image using {model_info()['model']}, this may take some time")
 
     #generate metadata
     response = await process_image(client, state["chat_id"], state["img_msg_id"])
-    await event.respond(response)
+    # await event.respond(response)
+    await event.respond("Generated markdown from image.")
     user_state[event.sender_id] = {
         "step": "ask_obsidian_vault",
         "markdown": response,
@@ -62,39 +75,49 @@ async def handle_image(event):
         buttons.append(Button.inline(vault.name, f"{vault.name}".encode()))
     await event.respond("Step 2) Vault selection", buttons=buttons)
 
-# @client.on(events.NewMessage)
-# async def handle_followup(event):
-#     state = user_state.get(event.sender_id)
-#     if not state:
-#         await event.respond("User state err")
-#         return
-#
-#     if state["step"] == "generate_metadata":
-#         answer = event.text.lower()
-#
-#         if answer.lower() in ['b', 'ai']:
-#             await event.respond(f"You have selected to use AI ({model_info()['model']})")
-#             await event.respond("Working with AI...")
-#             response = await process_image(client, state["chat_id"], state["img_msg_id"])
-#             await event.respond(response)
-#             user_state[event.sender_id] = {
-#                 "step": "ask_obsidian_vault",
-#                 "markdown": response,
-#             }
-#             state = user_state.get(event.sender_id)
-#         elif answer.lower() in ['a', 'tesseract']:
-#             await event.respond("You have selected to use tesseract")
-#             text = await ocr_image_from_telethon(client, state["chat_id"], state["img_msg_id"])
-#             await event.respond(text)
-#         # else:
-#             # await event.respond("Response not in options")
-#     if state["step"] == "ask_obsidian_vault":
-#         buttons = []
-#         usr_vaults = Settings.load().vaults
-#         for vault in usr_vaults:
-#             buttons.append(Button.inline(vault.name, f"{vault.name}".encode()))
-#         await event.respond("Step 2) Vault selection", buttons=buttons)
+@client.on(events.NewMessage)
+async def handle_followup(event):
+    state = user_state.get(event.sender_id)
+    text = event.text
+    if not state:
+        await event.respond("User state err")
+        return
 
+    # after user clicks the settings button
+    if state["step"] == "settings" and state["function"] != "":
+        match state["function"]:
+            case "change_model":
+                await event.respond(f"Setting model to {text}")
+                obsidian.settings.ai_model = text
+                obsidian.settings.save()
+                await event.respond(f"Done! Set model to {text}")
+            case "change_endpoint":
+                await event.respond(f"Changing Ollama endpoint to {text}...")
+                obsidian.settings.ai_endpoint = text
+                obsidian.settings.save()
+                await event.respond(f"Done! Set model to {text}")
+    if state["step"] == "settings" and state["function"] == "create_vault":
+        name = ""
+        path = ""
+        match state["substep"]:
+            case "vault_name": # After user inputs the name of the vault
+                name = text
+                await event.respond(f"Name: {name}\nWhere would you like to store this on your computer? Give an absolute path")
+                user_state[event.sender_id] = {
+                    "step": "settings",
+                    "function": "create_vault",
+                    "substep": "vault_path"
+                }
+            case "vault_path":
+                path = text
+                obsidian.settings.create_vault(name, path)
+                await event.respond(f"Created vault '{name}' at path '{path}'")
+
+        # user_state[event.sender_id] = {
+        #     "step": "settings",
+        #     "function": "create_vault",
+        #     "substep": ""
+        # }
 
 @client.on(events.CallbackQuery)
 async def handle_button(event):
@@ -108,10 +131,62 @@ async def handle_button(event):
 
         await event.respond(f"Selected '{vault_name}', generating file data (title, filename, etc)")
         metadata = await obsidian.generate_file_data(vault, state["markdown"])
-        await event.respond(f"Metadata:\n{metadata}")
         await event.respond("Adding to Vault...")
         obsidian.create_note(vault, state["markdown"], metadata['filename'], metadata['tags'])
         await event.respond(f"Created note {metadata['filename']}.md to vault '{vault_name}'")
+    elif state["step"] == "settings":
+        # change model, change endpoint, add vault
+        match data.decode().strip().lower():
+            case "change model":
+                await event.respond(f"""Ok, let's change the model.
+Please type in the exact model name.
+ For example, Type 'qwen2.5vl:3b' for the 'qwen2.5vl:3b' model.
+This should be the same thing you type to download models with ollama:
+```ollama run <model-name>``` or ```ollama pull <model name>```
+For a list of all models installed on your machine, run ```ollama list``` in your terminal.
+Make sure you have the model actually installed!
+You are currently using {obsidian.settings.ai_model}
+Which model would you like to change to?""")
+                user_state[event.sender_id] = {
+                    "step": "settings",
+                    "function": "change_model"
+                }
+
+
+            case "change endpoint":
+                await event.respond(f"""You selected to change the endpoint. 
+You are currently using
+```{obsidian.settings.ai_endpoint}```
+
+Ollama's default is 
+```http://localhost:11434```
+
+Your endpoint should start with 'http://' or 'https://'. An example would be:
+```http://123.45.678.90:11434```
+
+Please type the endpoint you'd like to use. 
+""")
+                user_state[event.sender_id] = {
+                    "step": "settings",
+                    "function": "change_endpoint"
+                }
+
+
+            case "create vault":
+                vaults = ""
+                for vault in obsidian.settings.list_vaults():
+                    name = vault[0]
+                    vaults = vaults + "    __" + name + "__\n"
+                await event.respond(f"""You selected to add a vault.
+Your current vaults are:
+{vaults}
+""")
+                await event.respond("What would you like to name your vault? This will be the folder name.")
+                user_state[event.sender_id] = {
+                    "step": "settings",
+                    "function": "create_vault",
+                    "substep": "vault_name"
+                }
 
 
 
